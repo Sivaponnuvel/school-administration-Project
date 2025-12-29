d64f23f353af77bed542e8d6f111c006c061f4e5
Sivaponnuvel <sivaponnuvel7@gmail.com>
2025-12-30 00:55:32 +0530

    Created by sidebar's

--- school_administration.py
+++ school_administration.py
@@ -11,3 +11,16 @@ con = mysql.connector.connect(
         database="school_administration"
 )
 sd = con.cursor()
+
+st.sidebar.title("School Administration")
+
+with st.sidebar:
+    selected = option_menu("Main Menu",["Home page","Student","Teacher"])
+
+    if selected == "Student":
+        st.sidebar.title("Student")
+        stu_option = option_menu("",["Signup","Login"])
+
+    elif selected == "Teacher":
+        st.sidebar.title("Teacher")
+        tec_option = option_menu("",["Signup","Login"])
