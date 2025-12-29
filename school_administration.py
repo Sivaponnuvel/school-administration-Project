5a6c33d46a1ce0c3c3faeea958bfae9a9f68b9ee
Sivaponnuvel <sivaponnuvel7@gmail.com>
2025-12-29 12:52:18 +0530

    connect the database

--- school_administration.py
+++ school_administration.py
@@ -3,3 +3,11 @@ from tabulate import tabulate
 import streamlit as st
 from streamlit_option_menu import option_menu
 import pandas as pd
+
+con = mysql.connector.connect(
+        host="localhost",
+        username="root",
+        password="siva2025",
+        database="school_administration"
+)
+sd = con.cursor()
